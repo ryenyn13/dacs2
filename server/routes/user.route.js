9ad7028ba2ca05eec73128ef13821dae7f70c2a4
RyeNyn13_ <146347486+ryenyn13@users.noreply.github.com>
2024-12-24 06:30:09 +0700

    contact chưa hiện được

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,6 +2,8 @@ import express from "express";
 import {protectRoute} from "../middleware/protectRoute.js";
 import {
     getAllUsers,
+    getUser,
+    updateUser,
     getAllCart,
     addToCart,
     deleteCart,
@@ -12,6 +14,8 @@ import {
 const router = express.Router();
 
 router.get("/all", getAllUsers);
+router.get("/getUser/:id", getUser);
+router.post("/update/:id", updateUser);
 router.get("/cart", protectRoute, getAllCart);
 router.delete("/delete/cart/all", protectRoute, deleteAllCart);
 router.post("/add/cart/:id", protectRoute, addToCart);
